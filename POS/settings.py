d36d1e434ecd2a21a6abc155b80d0aceb32fd58c
adamsfey3@gmail.com <adamsfey3@gmail.com>
2021-10-12 16:14:15 +0300

    :gear:

--- POS/settings.py
+++ POS/settings.py
@@ -37,12 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    
     'payments',
     'inventory',
     'accounts',
-
+ 
     'rest_framework',
+    'crispy_froms'
 
 ]
 
